433f39591ba29f38c10afd5cce9a03ca266bb39f
Neeha-shirin <neehasherin987@gmail.com>
2025-11-01 15:40:52 +0530

    Initial project commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 Add base here — MUST match your repo name
 export default defineConfig({
   plugins: [react()],
- 
+  base: '/'   // important for Netlify routing
 })
