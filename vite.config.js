7105d5bb5cbbee646fceb583b558bb70066737ae
Neeha-shirin <neehasherin987@gmail.com>
2025-11-01 15:16:32 +0530

    Initial project commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // 👇 Add base here — MUST match your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/react-project/', // <-- replace with '/react-p-/' if that’s your repo
+ 
 })
